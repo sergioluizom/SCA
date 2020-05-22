1b5c5954430a6c73903868078960d8c7a6e04bf2
Sergio Luiz de Oliveira Machado <sergioluizom@gmail.com>
2020-05-22 17:39:19 -0300

    Alterações

--- SCA.Service/Implementation/UserService.cs
+++ SCA.Service/Implementation/UserService.cs
@@ -12,19 +12,18 @@ namespace SCA.Service.Implementation
     public class UserService : IUserService
     {
         private readonly IUserRepository userRepository;
-        private readonly IRabbitMQ rabbitMQ;
-        private const string QueueUserAdd = "QueueUserAdd";
-        private const string QueueUserUpdate = "QueueUserUpdate";
-        private const string QueueUserDelete = "QueueUserDelete";
-        public UserService(IUserRepository userRepository, IRabbitMQ rabbitMQ)
+        public UserService(IUserRepository userRepository)
         {
             this.userRepository = userRepository;
-            this.rabbitMQ = rabbitMQ;
         }
 
-        public async Task<bool> Add(User user) => await rabbitMQ.WriteMessageOnQueue(JsonConvert.SerializeObject(user), QueueUserAdd);
-        public async Task<bool> Update(User user) => await rabbitMQ.WriteMessageOnQueue(JsonConvert.SerializeObject(user), QueueUserUpdate);
-        public async Task<bool> Delete(string id) => await rabbitMQ.WriteMessageOnQueue(JsonConvert.SerializeObject(id), QueueUserDelete);
+        public async Task<bool> Add(User user)
+        {
+            
+            await userRepository.Add(user);
+        }
+        public async Task<bool> Update(User user) => await userRepository.Update(user);
+        public async Task<bool> Delete(string id) => await userRepository.Delete(id);
         public async Task<User> Find(string id) => await userRepository.Find(id);
         public async Task<List<User>> FindByCriteria(UserSearchModel user) => await userRepository.FindByCriteria(user);
     }
---
+++ SCA/Controllers/SecurityController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SCA.API.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class SecurityController : ControllerBase
+    {
+    }
+}
